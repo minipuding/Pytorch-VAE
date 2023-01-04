d66e27063cea8f03d88dfbd8c61f5a4fc3fc89df
minipuding <sijie.x@foxmail.com>
2023-01-04 20:32:01 +0800

    Code reading by adding annotations.

--- models/base.py
+++ models/base.py
@@ -1,8 +1,13 @@
-from .types_ import *
+from .types_ import * # 为了方便，作者直接将所有用到的类型在``type_.py``文件中导入
 from torch import nn
 from abc import abstractmethod
 
+
 class BaseVAE(nn.Module):
+    """
+    建立VAE基类，从成员函数可以看出，VAE主要由编码器``encode(r)``、解码器``decode(r)``、
+    采样器``sample(r)``、生成器``generate``四个部分组成。
+    """
     
     def __init__(self) -> None:
         super(BaseVAE, self).__init__()
